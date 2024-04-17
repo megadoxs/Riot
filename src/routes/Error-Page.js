050844efe4205986a9b91a77931ea7d731616267
megadoxs <megadoxs@gmail.com>
2024-04-17 17:20:47 -0400

    initialization

---
+++ src/routes/Error-Page.js
@@ -0,0 +1,18 @@
+import { useRouteError } from "react-router-dom";
+import './Error-Page.css';
+
+// error page to design
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  );
+}
---
+++ src/routes/Root.js
@@ -0,0 +1,10 @@
+import './Root.css';
+
+// page that will be loaded by the user on load
+export default function Root() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
